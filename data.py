0aa80d692dc3bfc6bf520801128d296dbed232c8
CharlotteHoyt <charlotte.hoyt@comcast.net>
2025-12-15 01:21:51 -0800

    Collect and display counts for all columns.

--- data.py
+++ data.py
@@ -37,39 +37,87 @@ for value in skin_tone_values:
 # Eye Color
 print(Fore.BLACK + Fore.MAGENTA + "\nEYE COLOR:")
 eye_color_values = []
+eye_color_values_count = []
 
 for value in df['Eyes'].tolist():
-    eye_color_values.append(str(value))
+    if value not in eye_color_values:
+        eye_color_values.append(str(value))
+        eye_color_values_count.append(1)
+    else:
+        index = eye_color_values.index(value)
+        eye_color_values_count[index] += 1
 
-eye_color_output = ', '.join(eye_color_values)
-print(eye_color_output)
+# print(eye_color_values)
+# print(eye_color_values_count)
+
+index = 0
+for value in eye_color_values:
+    if not (value == "nan"):
+        print(f"{value}: {eye_color_values_count[index]}")
+    index += 1
 
 # Hair
 print(Fore.BLACK + Fore.MAGENTA + "\nHAIR:")
 hair_values = []
+hair_values_count = []
 
 for value in df['Hair'].tolist():
-    hair_values.append(str(value))
+    if value not in hair_values:
+        hair_values.append(str(value))
+        hair_values_count.append(1)
+    else:
+        index = hair_values.index(value)
+        hair_values_count[index] += 1
 
-hair_output = ', '.join(hair_values)
-print(hair_output)
+# print(hair_values)
+# print(hair_values_count)
+
+index = 0
+for value in hair_values:
+    if not (value == "nan"):
+        print(f"{value}: {hair_values_count[index]}")
+    index += 1
 
 # Gender
 print(Fore.BLACK + Fore.MAGENTA + "\nGENDER:")
 gender_values = []
+gender_values_count = []
 
 for value in df['Gender'].tolist():
-    gender_values.append(str(value))
+    if value not in gender_values:
+        gender_values.append(str(value))
+        gender_values_count.append(1)
+    else:
+        index = gender_values.index(value)
+        gender_values_count[index] += 1
 
-gender_output = ', '.join(gender_values)
-print(gender_output)
+# print(gender_values)
+# print(gender_values_count)
+
+index = 0
+for value in gender_values:
+    if not (value == "nan"):
+        print(f"{value}: {gender_values_count[index]}")
+    index += 1
 
 # Race (Filter by Human)
 print(Fore.BLACK + Fore.MAGENTA + "\nRACE:")
 race_values = []
+race_values_count = []
 
 for value in df['Race'].tolist():
-    race_values.append(str(value))
+    if value not in race_values:
+        race_values.append(str(value))
+        race_values_count.append(1)
+    else:
+        index = race_values.index(value)
+        race_values_count[index] += 1
 
-race_output = ', '.join(race_values)
-print(race_output)
+# print(race_values)
+# print(race_values_count)
+
+index = 0
+for value in race_values:
+    if not (value == "nan"):
+        print(f"{value}: {race_values_count[index]}")
+    index += 1
